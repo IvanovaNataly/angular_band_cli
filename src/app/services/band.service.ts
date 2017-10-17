d29e8437f3036aa970078392ede75e92d9e41b19
NETCRAFT\nataliai <nataliai@netcraft.co.il>
2017-10-17 15:04:54 +0300

    resolve is saved to variable in service

--- src/app/services/band.service.ts
+++ src/app/services/band.service.ts
@@ -6,10 +6,26 @@ import SingerModel from "../models/singerModel";
 @Injectable()
 export default class BandService {
     endpoint: string;
+    singers: SingerModel[];
 
     constructor(private httpClient: HttpClient) {
         this.endpoint = "http://localhost:3000";
     }
+
+    // A version to save response to variable;
+    // getSingers() {
+    //     const p: Promise<SingerModel[]> = new Promise((resolve, reject) => {
+    //         this.httpClient.get(`${this.endpoint}/bands`).subscribe(response => resolve(response),
+    //                                                                                     error => reject(error));
+    //     });
+    //     p.then( val => {
+    //         this.singers = val;
+    //         console.log(this.singers);
+    //         return this.singers;
+    //     },
+    //         err => {console.log("Error in Promise", err);}
+    //     );
+    // }
 
     getSingers(): Promise<SingerModel[]> {
         return new Promise((resolve, reject) => {
